7ae4e1222c5da64374b78bc05d23c93e19bb35af
Krystal <krystal.raysor@outlook.com>
2019-12-16 12:04:56 -0500

    Correct manhattan distance

--- image_blur_3.rb
+++ image_blur_3.rb
@@ -1,59 +1,55 @@
 class Image
-    def initialize(image)
-      @image = image
-    end
-  
-    def output_image
-          @image.each do |row|
-              puts row.join
-        end
-    end
-  
-   def blur!(distance)
-      distance.times do
-      anchor.each do |x, y|
-        dilation x, y
+  def initialize(image)
+    @image = image
+  end
+
+  def output_image
+        @image.each do |row|
+            puts row.join
+      end
+  end
+
+ def blur!(distance)
+  ones = anchor
+  @image.each_with_index do |row, row_number|
+    row.each_with_index do |item, col_number|
+      ones.each do |found_row_number, found_col_number|
+        if manhattan_distance(col_number, row_number, found_col_number, found_row_number) <= distance
+          @image[row_number][col_number] = 1
         end
       end
-    end
-  
-    def anchor
-      array = []
-      @image.each.with_index do |row, x|
-        row.each.with_index do |pixel, y|
-          array << [x, y] if pixel == 1
-        end
-      end
-      array
-    end
-    
-    def dilation(x, y)
-      strel x + 1, y # down one
-      strel x - 1, y # up one
-      strel x, y + 1 # right one
-      strel x, y - 1 # left one
-      strel x - 1, y + 1 # right top diagonal
-      strel x - 1, y - 1 # left top diagonal
-      strel x + 1, y + 1 # right bottom diagonal
-      strel x + 1, y - 1 # left bottom diagonal
-    end
-  
-    def strel(x, y)
-      return unless x >= 0 && x < @image[0].length - 1
-      return unless y >= 0 && y < @image.length 
-      @image[x][y] = 1 
     end
   end
+end
+
+  def anchor
+    array = []
+    @image.each.with_index do |row, x|
+      row.each.with_index do |pixel, y|
+        array << [x, y] if pixel == 1
+      end
+    end
+    array
+  end
   
-  image = Image.new([
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0, 0], 
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0]
-  ])
-    
-  image.blur!(2)
-  image.output_image
+   def manhattan_distance (x1, y1, x2, y2)
+      horizontal_distance = (x2 - x1).abs
+      vertical_distance = (y2 - y1).abs
+      # (x2 - x1).abs + (y2 - y1).abs
+      horizontal_distance + vertical_distance
+  end
+
+end
+
+image = Image.new([
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 0, 0], 
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 1]
+])
+
+image.blur!(2)
+image.output_image
